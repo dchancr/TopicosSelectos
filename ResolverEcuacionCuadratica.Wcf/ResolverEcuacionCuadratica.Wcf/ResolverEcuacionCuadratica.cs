56b6fff31129633f8e9ba142f42c8cbe392ae6b7
dchancr <chancr_3@hotmail.com>
2015-12-14 21:25:13 -0600

    update

---
+++ ResolverEcuacionCuadratica.Wcf/ResolverEcuacionCuadratica.Wcf/ResolverEcuacionCuadratica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using REcuacionCuadratica;
+
+namespace REcuacionCuadratica.Wcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ResolverEcuacionCuadratica : IResolverEcuacionCuadratica
+    {
+        public void ResolverLaEcuacionCuadratica(Double Primer,Double Segundo,Double Tercer,Double Discriminante,ref Double Raiz1, ref Double Raiz2)
+        {
+             ResolverEcuacionCuadratica resolverEcuacion = new ResolverEcuacionCuadratica();
+
+            resolverEcuacion.ResolverLaEcuacionCuadratica(Primer,Segundo,Tercer,Discriminante,ref Raiz1, ref Raiz2);
+        }
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ ResolverEcuacionCuadratica/WCFResolverEcuacionCuadratica/IResolverEcuacionCuadratica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFResolverEcuacionCuadratica
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IResolverEcuacionCuadratica
+    {
+        [OperationContract]
+        void resolverLaEcuacionCuadratica(Double Primer, Double Segundo, Double Tercer, Double Discriminante, ref Double Raiz1, ref Double Raiz2);
+       
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
